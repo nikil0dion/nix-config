f3a45f715f4bb10e8c69abf2df5da35ad9a62d12
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-09 16:12:36 +0300

    Create hardware-configuration.nix

---
+++ modules/hardware-configuration.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, modulesPath, ... }: 
+
+{ 
+  import = [ ];
+ 
+  boot.tmp.cleanOnBoot = true;
+  boot.tmp.tmpfsSize = "5GB";
+  boot.initrd.availableKernelModules = [
+    "ahci"      # SATA controller
+    "nvme"      # NVMe SSD support
+    "xhci_pci"  # USB 3.0/3.1
+    "amdgpu"    # AMD GPU driver (for Ryzen APUs) 
+    ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+  # Bootloader 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  fileSystems."/" = 
+    { device = "dev/disk/by-label/NIXROOT";
+      fsType = "btrfs";
+    };
+
+  fileSystems."/boot" = 
+    { device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat"; 
+    };
+
+   swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 8196;
+    }
+  ];
+}
