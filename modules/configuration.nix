06bc9c0d87d68bdea1a6d29d70e7ada44aa54c81
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-09 15:14:37 +0300

    Create configuration.nix

---
+++ modules/configuration.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ...}:
+
+{ 
+  imports = [
+      ./hardware-configuration.nix
+      ./packages.nix
+    ];
+
+ # Bootloader 
+ boot.loader.systemd-boot.enable = true;
+ boot.loader.efi.canTouchEfiVariables = true;
+
+ # Your hostname
+ networking.hostName = "nix";
+
+ # Enable networking 
+ networking.networkmanager.enable = true;
+
+ # Set time zone
+ time.timeZone = "UTC";
+
+ # Select internationalisation propertis
+ i18n.defaultLocale = "en_US.UTF-8";
+
+ # Define a user accout 
+ users.user.nix = { 
+    isNormalUser = true; 
+    description = "nix";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = import ./packages.nix;
+};
+
+ # Enable nix Flakes
+ nix.settings.experimental-features = [ "nix-command" "flakes" ]
+ 
+ # Hyperland enable
+ hyperland.enable = true;
+
+ # System version 
+ system.stateVersion = "24.11";
+}
