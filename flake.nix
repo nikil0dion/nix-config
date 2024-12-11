9793c9041c353797fd56f6dedb01f7ef88568bc0
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-11 17:02:55 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,45 +1,43 @@
-{  
-  description = "My nixos configuration";
+{
+  description = "My NixOS configuration";
 
-  inputs = { 
-      # NixOS defaults 
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-      systems.url = "github:nix-systems/default-linux";
-      hardware.url = "github:nixos/nixos-hardware";
-      #disco.url = "github:nix-community/disco";
-      #disco.inputs.nixpkgs.follows = "nixpkgs";
-      impermanence.url = "github:nix-community/impormanence";
+  inputs = {
+    # NixOS defaults
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default-linux";
+    hardware.url = "github:nixos/nixos-hardware";
+    impermanence.url = "github:nix-community/impermanence";
 
-      # Home configurations
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Home configurations
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-      # Secureboot
-      #lanzabote.url = "github:nix-community/lanzabote/v0.4.1";
-      #lanzabote.inputs.nixpkgs.follows = "nixpkgs";
+    # Secureboot
+    #lanzabote.url = "github:nix-community/lanzabote/v0.4.1";
+    #lanzabote.inputs.nixpkgs.follows = "nixpkgs";
 
-      # User Theme
-      catppuccin.url = "github:catppuccin/nix";
+    # User Theme
+    catppuccin.url = "github:catppuccin/nix";
 
-      # SSH secrets app
-      aginix.url = "github:ryantm/agenix";
-      aginix.inputs.nixpkgs.follows = "nixpkgs";
+    # SSH secrets app
+    aginix.url = "github:ryantm/agenix";
+    aginix.inputs.nixpkgs.follows = "nixpkgs";
 
-      # Hyperland
-      #hyperland.url = "git+https://github.com/hyprwm/Hyperland?submodules=1";
-   }
+    # Hyperland
+    #hyperland.url = "git+https://github.com/hyprwm/Hyperland?submodules=1";
+  };
 
- outputs = {self, nixpkgs, home-manager, ... }@inputs: 
-      
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; }; 
-      linuxSystems = [ "x86_64-linux" ];
-      modules = [ 
-         ./configuration.nix
-         ./linux-kernel.nix
-         ./hardware-configuration.nix
-         ./display-manager.nix
-         ];
-   }      
-    
+      specialArgs = { inherit inputs; };
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./linux-kernel.nix
+        ./hardware-configuration.nix
+        ./display-manager.nix
+      ];
+    };
+  };
+}
