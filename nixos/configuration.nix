4ed14d512e1c7f234bc53f28c7caa85065e7adeb
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-25 14:07:08 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -3,24 +3,8 @@
 { 
   imports = [
       ./hardware-configuration.nix
-      ./packages.nix
       ./linux-kernel.nix
-      ./security.nix
-      ./programs.nix
-      ./services.nix
     ];
-
- # Env defaults
- environment.sessionVariables = {
-   NIXOS_OZONE_WL = "1";
-   WLR_NO_HARDWARE_CURSORS = "1";
-  # XDG_CACHE_HOME = "$HOME/.cache";   
-  # XDG_CONFIG_HOME = "$HOME/.config";
-  # XDG_DATA_HOME = "$HOME/.local/share";
-  # XDG_STATE_HOME = "$HOME/.local/state";
-  # XDG_CURRENT_DESKTOP = "Hyprland";
-  # XDG_SESSION_DESKTOP = "Hyprland";
-};
 
  # Your hostname
  networking.hostName = "nix";
@@ -28,7 +12,7 @@
  # Enable networking 
  networking.networkmanager.enable = true;
 
- # Set time zone
+ # Set time zonenanop
  time.timeZone = "UTC";
 
  # Select internationalisation propertis
@@ -39,15 +23,7 @@
     isNormalUser = true; 
     description = "nix";
     extraGroups = [ "networkmanager" ];
-};
-
- # Steam enable 
- programs.steam = {
-   enable = true;
-   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
- };
+};fp
 
  # Enable nix Flakes
  nix.settings.experimental-features = [ "nix-command" "flakes" ]
