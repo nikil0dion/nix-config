d48ee4f774293846f2fd5b0bcbe5b72b44e32653
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-27 15:56:21 +0300

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,22 +23,20 @@
  i18n.defaultLocale = "en_US.UTF-8";
 
 # Define a user accout 
- users.user.nixos = { 
+ users.users.nixos = { 
     isNormalUser = true; 
     description = "nix";
-    extraGroups = [ "networkmanager" ];
-};fp
+    extraGroups = [ "networkmanager" "wheel" ];
+};
 
  # Enable nix Flakes
- nix.settings.experimental-features = [ "nix-command" "flakes" ]
-
+ nix.settings.experimental-features = [ "nix-command" "flakes" ];
  # Fonts
   fonts.packages = with pkgs; [
     jetbrains-mono
     nerd-font-patcher
     noto-fonts-color-emoji
   ];
-}
 
    # Realtime Policy and Watchdog Daemon
    security.rtkit.enable = true;
@@ -55,25 +53,13 @@
   nixpkgs.config.allowUnfree = true;
 
   # Enable the general xdg-desktop-portal service
-  services.xdg-desktop-portal.enable = true;
+  xdg.autostart.enable = true;
 
-  # Enable xdg-desktop-portal-hyprland for Hyprland compatibility
-  services.xdg-desktop-portal-hyprland.enable = true;
-
-  # If you are using Hyprland as your compositor, make sure it's enabled:
-  services.xserver.windowManager.hyprland.enable = true;
-  
   # OpenGL
   hardware.opengl = {
     enable = true;  # Enable OpenGL
     driSupport = true;  # Enable DRI (Direct Rendering Infrastructure)
     driSupport32Bit = true;  # Enable 32-bit DRI support for compatibility
-  };
-
-# Vulkan support
-  hardware.vulkan = {
-    enable = true;
-    drivers = [ "amdvlk" ];  # Use AMD's Vulkan driver
   };
 
 # System version 
