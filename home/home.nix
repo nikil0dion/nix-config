f1b5e862ba629b68f2b48fe6bb4c3393593974b0
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-27 13:10:26 +0300

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -5,15 +5,106 @@
   ];
 
   home = {
-    username = "nixos";
-    homeDirectory = "/home/nixos";
-    stateVersion = "24.11";
-    sessionVariables = {
-        EDITOR = "nano";
-        BROWSER = "firefox";
-        NIXOS_OZONE_WL = "1";
-        TERMINAL = "wezterm";
-  };
+      username = "nixos";
+      homeDirectory = "/home/nixos";
+      stateVersion = "24.11";
+      sessionVariables = {
+          EDITOR = "nano";
+          BROWSER = "firefox";
+          NIXOS_OZONE_WL = "1";
+          TERMINAL = "wezterm";
+      };
+      packages = with pkgs; [
+          # Desktop apps
+          firefox
+          firefox-esr
+          telegram-desktop
+          brave
+          rofi
+          wofi
+          vlc
+          gparted
+          obsidian
+          handbrake
+
+          # Coding stuff
+          go
+          vscodium
+
+          # CLI utils
+          lsd
+          neofetch
+          file
+          tree
+          wget
+          git
+          fastfetch
+          htop
+          nix-index
+          unzip
+          scrot
+          ffmpeg
+          light
+          lux
+          mediainfo
+          ranger
+          zram-generator
+          cava
+          zip
+          brightnessctl
+          swww
+          openssl
+          lazygit
+          bluez
+          bluez-tools
+          docker
+          kubectl
+
+          # GUI utils
+          feh
+          imv
+          dmenu
+          screenkey
+          mako
+          gromit-mpx
+          httpie
+
+          # Wayland stuff
+          xwayland
+          wl-clipboard
+          cliphist
+      
+          # WMs and stuff
+          herbstluftwm
+          hyprland
+          seatd
+          xdg-desktop-portal-hyprland
+          polybar
+          waybar
+          pyprland
+          hyprpicker
+          hyprcursor
+          hyprlock
+          hypridle
+
+          # Sound
+          pipewire
+
+          # Screenshotting
+          grim
+          grimblast
+          slurp
+          flameshot
+          swappy
+
+          # Other
+          home-manager
+          spice-vdagent
+          tokyo-night-gtk
+          xray
+          nekoray
+        ];
+
 
   programs.bash = {
     enable = true;
