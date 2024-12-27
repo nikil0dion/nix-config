01c80e1d9ededf4e53dc2f19331fa28380371b33
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2024-12-27 14:12:24 +0300

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -140,13 +140,41 @@
  
   programs.wezterm.enable = true;
 
-  programs.hyprland = { 
+  wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
-  };
+    systemd.enable = true;
+    extraConfig =
+      let
+        modifier = "SUPER";
+      in
+      concatStrings [
+        ''
+          env = NIXOS_OZONE_WL, 1
+          env = NIXPKGS_ALLOW_UNFREE, 1
+          env = XDG_CURRENT_DESKTOP, Hyprland
+          env = XDG_SESSION_TYPE, wayland
+          env = XDG_SESSION_DESKTOP, Hyprland
+          env = GDK_BACKEND, wayland, x11
+          env = CLUTTER_BACKEND, wayland
+          env = QT_QPA_PLATFORM=wayland;xcb
+          env = QT_WAYLAND_DISABLE_WINDOWDECORATION, 1
+          env = QT_AUTO_SCREEN_SCALE_FACTOR, 1
+          env = SDL_VIDEODRIVER, x11
+          env = MOZ_ENABLE_WAYLAND, 1
+          exec-once = dbus-update-activation-environment --systemd --all
+          exec-once = systemctl --user import-environment QT_QPA_PLATFORMTHEME WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+          exec-once = killall -q swww;sleep .5 && swww init
+          exec-once = killall -q waybar;sleep .5 && waybar
+          exec-once = killall -q swaync;sleep .5 && swaync
+          exec-once = nm-applet --indicator
+          exec-once = lxqt-policykit-agent
+          exec-once = sleep 1.5 && swww img /home/nixos/wallpapers/mountains-1.jpg
+
 
   
   
+  programs.dcof.enable = true;
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
