50e72a687b3526669394bdab342cc4eb0749857a
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2025-04-22 09:15:58 +0300

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -7,31 +7,42 @@
       homeDirectory = "/home/nixos";
       stateVersion = "24.11";
       packages = with pkgs; [
-         gimp
+         audacity
          bluez
+         bottles-unwrapped
          cachix
          cursor
          dconf
          dconf-editor
          docker
          docker-compose
+         drawio
+         ffmpeg-full
+         file
          floorp
          gcc-wrapper
+         gimp
          gnome-tweaks
          gnumake
+         gst-plugins-base
+         gst-plugins-good
+         gstreamer
          kdenlive
          keepassxc
          kubectl
          lsd
+         lsof
          meld
          minikube
          nekoray
-         neofetch
+         neofetch-unstable
          obsidian
          pgadmin
+         python3.12-pip
          steam
          telegram-desktop
          tokyonight-gtk-theme-0-unstable
+         vlc
          vulkan-tools
          warp-terminal
       ];
