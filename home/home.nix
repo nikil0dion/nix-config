5ddc12efa09e3cb4d07e4b7ffa3ea9cc7b07f3f4
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2025-07-22 10:41:28 +0300

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -5,46 +5,62 @@
   home = {
       username = "nixos";
       homeDirectory = "/home/nixos";
-      stateVersion = "24.11";
+      stateVersion = "25.05";
       packages = with pkgs; [
-         audacity
+         alsa-utils
+	       audacity
+	       appimage-run
          bluez
          bottles-unwrapped
          cachix
+	       clickhouse
          cursor
          dconf
+         dive
+         dbeaver-bin
          dconf-editor
          docker
          docker-compose
          drawio
          ffmpeg-full
+         figma-linux
          file
          floorp
          gcc-wrapper
          gimp
          gnome-tweaks
          gnumake
+         grpcurl
          gst-plugins-base
          gst-plugins-good
          gstreamer
+         hamster
+         httpie-desktop
          kdenlive
          keepassxc
          kubectl
          lsd
          lsof
+         lynis
          meld
          minikube
+         minio-client-
          nekoray
          neofetch-unstable
          obsidian
          pgadmin
+         postgresql
+         pciutils
          python3.12-pip
          steam
+         sdkmanager
          telegram-desktop
          tokyonight-gtk-theme-0-unstable
          vlc
          vulkan-tools
          warp-terminal
+	       wget
+         zip
       ];
   };
 }
