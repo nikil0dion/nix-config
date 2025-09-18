1acc97c69ac79a4c04c19d0c12ad2fcdd8b10f5c
nikil0dion <152965868+nikil0dion@users.noreply.github.com>
2025-09-18 12:54:08 +0300

    Update hardware-configuration.nix

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,11 +4,16 @@
   imports = [ ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
-  boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ "msr" "i915" "coretemp" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
   boot.blacklistedKernelModules = [ "kvm_intel" ];
   boot.tmp.cleanOnBoot = true;
+  # hibernation luks
+  boot.resumeDevice = "/dev/disk/by-uuid/c390e2e3-8687-428f-8443-18c43218273f";
+  boot.kernelParams = [ "resume_offset=1320192" ]; 
+  boot.initrd.systemd.enable = true;
+  boot.initrd.supportedFilesystems = [ "btrfs" ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/d803212c-104c-4157-bf10-6dbe315115b5";
